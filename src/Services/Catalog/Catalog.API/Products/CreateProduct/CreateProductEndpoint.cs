2664eeedf8b4ac9978609576c3b74e89ba231dc8
James Angelo Sarona <jacsarona.official@gmail.com>
2025-02-23 22:20:39 +0800

    Refactor: Change reference and packages to BuildingBlocks

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,5 +1,15 @@
-﻿namespace Catalog.API.Products.CreateProduct;
-public class CreateProductEndpoint
+﻿using Carter;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+
+public record CreateProductResponse(Guid Id);
+
+public class CreateProductEndpoint : ICarterModule
 {
-        
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        throw new NotImplementedException();
+    }
 } 
