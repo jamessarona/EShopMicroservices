eb03238ad336ad828fc8df76c879d32ed734f124
James Angelo Sarona <jacsarona.official@gmail.com>
2025-02-25 21:10:54 +0800

    FIx: change incorrect endpoint name

--- src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -5,7 +5,7 @@ public class GetProductByCategoryEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("/procuts/category/${category}", async (string category, ISender sender) =>
+        app.MapGet("/products/category/{category}", async (string category, ISender sender) =>
         {
             var result = await sender.Send(new GetProductByCategoryQuery(category));
             var response = result.Adapt<GetProductByCategoryResponse>();
--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -1,9 +1,5 @@
-﻿
-using Catalog.API.Products.GetProducts;
+﻿namespace Catalog.API.Products.GetProductById;
 
-namespace Catalog.API.Products.GetProductById;
-
-// public record GetProductByIdRequest();
 public record GetProductByIdResponse(Product Product);
 
 public class GetProductByIdEndpoint : ICarterModule
