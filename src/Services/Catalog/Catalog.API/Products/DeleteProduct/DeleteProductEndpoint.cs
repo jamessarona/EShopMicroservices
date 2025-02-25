18d4e7fd2d3ba2749981134d4d0df49a3db9ad85
James Angelo Sarona <jacsarona.official@gmail.com>
2025-02-25 21:55:02 +0800

    Fix: lacking of parameter

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -1,7 +1,8 @@
 ﻿namespace Catalog.API.Products.DeleteProduct;
 
-// public record DeleteProductRequest(Guid Id);
+//public record DeleteProductRequest(Guid Id);
 public record DeleteProductResponse(bool IsSuccess);
+
 public class DeleteProductEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
@@ -12,7 +13,7 @@ public class DeleteProductEndpoint : ICarterModule
 
             var response = result.Adapt<DeleteProductResponse>();
 
-            return Results.Ok;
+            return Results.Ok(response);
         })
         .WithName("DeleteProduct")
         .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
