2703a2a5fca6ca38ce8fdef2338c87f27627cd4e
James Angelo Sarona <jacsarona.official@gmail.com>
2025-02-26 18:41:50 +0800

    Fix: inccorect usage of interface

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -16,17 +16,12 @@ public class CreateProductCommandValidator : AbstractValidator<CreateProductComm
 }
 
 internal class CreateProductCommandHandler
-        (IDocumentSession session, IValidator<CreateProductCommand> validator)
+        (IDocumentSession session, ILogger<CreateProductCommandHandler> logger)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        //var result = await validator.ValidateAsync(command, cancellationToken);
-        //var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
-        //if (errors.Any())
-        //{
-        //    throw new ValidationException(errors.FirstOrDefault());
-        //}
+        logger.LogInformation("CreateProductCommandHandler.Handle called with {@Command}", command);
 
         var product = new Product
         {
