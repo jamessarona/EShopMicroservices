2a9ea2ebb1778ba7d99571517d702599db7a6ddb
James Angelo Sarona <jacsarona.official@gmail.com>
2025-02-26 18:42:31 +0800

    Refactor: declare variable for typeof(Program).Assembly

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -3,14 +3,16 @@ using BuildingBlocks.Behaviors;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddCarter();
+
+var assembly = typeof(Program).Assembly;
 builder.Services.AddMediatR(config =>
 {
-    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+    config.RegisterServicesFromAssemblies(assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
+builder.Services.AddValidatorsFromAssembly(assembly);
 
-builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+builder.Services.AddCarter();
 
 builder.Services.AddMarten(opts =>
 {
