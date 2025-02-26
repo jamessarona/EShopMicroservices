0569ee179d8c297d2047ea79a05eb5a8934bd3dc
James Angelo Sarona <jacsarona.official@gmail.com>
2025-02-26 21:16:54 +0800

    Fix: invalid usage record

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,15 +1,15 @@
 ﻿namespace Catalog.API.Products.GetProducts;
 
-public record GetProductRequest(int? PageNumber = 1, int? PageSize = 10);
+public record GetProductsRequest(int? PageNumber = 1, int? PageSize = 10);
 public record GetProductResponse(IEnumerable<Product> Products);
 
 public class GetProductsEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("/products", async ([AsParameters] GetProductRequest request, ISender sender) =>
+        app.MapGet("/products", async ([AsParameters] GetProductsRequest request, ISender sender) =>
         {
-            var query = request.Adapt<GetProductRequest>();
+            var query = request.Adapt<GetProductsQuery>();
             var result = await sender.Send(query);
             var response = result.Adapt<GetProductResponse>();
 
