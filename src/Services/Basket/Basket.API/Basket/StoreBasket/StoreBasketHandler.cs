c4906463c1c95caaeb373b4aa6b880cc1db8c71d
James Angelo Sarona <jacsarona.official@gmail.com>
2025-02-28 20:32:11 +0800

    feat: expose store basket endpoint

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,4 +1,16 @@
 ﻿namespace Basket.API.Basket.StoreBasket;
-public class StoreBasketHandler
+
+public record  GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+
+public record GetBasketResult(ShoppingCart ShoppingCart);
+
+public class StoreBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
 {
+    public async Task<GetBasketResult> Handle(GetBasketQuery request, CancellationToken cancellationToken)
+    {
+        //TODO: get basket from database
+        //var basket = await _repository.GetBasket(request.UserName);
+
+        return new GetBasketResult(new ShoppingCart("swn"));
+    }
 }
