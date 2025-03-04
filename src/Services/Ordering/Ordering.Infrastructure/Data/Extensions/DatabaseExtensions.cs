c1f897866d0968f3139e949ab837d9b6cb02d37a
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-04 17:09:01 +0800

    feat: add seeding of product and order with items initial data

--- src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -18,7 +18,9 @@ public static class DatabaseExtensions
 
     private static async Task SeedAsync(ApplicationDbContext context)
     {
-        await SeedCustomerAsync(context); 
+        await SeedCustomerAsync(context);
+        await SeedProductAsync(context);
+        await SeedOrderandItemsAsync(context);
     }
 
     private static async Task SeedCustomerAsync(ApplicationDbContext context)
@@ -29,5 +31,22 @@ public static class DatabaseExtensions
             await context.SaveChangesAsync();
         }
     }
+
+    private static async Task SeedProductAsync(ApplicationDbContext context)
+    {
+        if (!await context.Products.AnyAsync())
+        {
+            await context.Products.AddRangeAsync(InitialData.Products);
+            await context.SaveChangesAsync();
+        }
+    }
+
+    private static async Task SeedOrderandItemsAsync(ApplicationDbContext context)
+    {
+        if (!await context.Orders.AnyAsync())
+        {
+            await context.Orders.AddRangeAsync(InitialData.OrdersWithItems);
+            await context.SaveChangesAsync();
+        }
     }
 }
