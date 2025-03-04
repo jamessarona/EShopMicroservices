ce894217c58d75f70ce3fe8cbb9fb1822d9c68e0
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-04 16:58:30 +0800

    feat: add auto migrate if environemnt is development

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,7 @@
 using Ordering.API;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,10 +14,16 @@ builder.Services
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseApiServices();
+
+if(app.Environment.IsDevelopment())
+{
+    await app.InitializeDatabaseAsync();
+}
 
 app.Run();
 
 
-// Run in Package Manager Console for Migration Command
+// Run in Package Manager Console for Manual Migration Command
 // Add-Migration InitialCreate -OutputDir Data/Migrations -Project Ordering.Infrastructure -StartupProject Ordering.API
 // Update-Database
