168538fc04233969decc3801423ead3ef0d96cd9
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-04 14:22:09 +0800

    refactor: fix incorrect casing of method name

--- src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -5,7 +5,7 @@ public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
     private readonly List<IDomainEvent> _domainEvents = new();
     public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
 
-    public void AddDOmainEvent(IDomainEvent domainEvent)
+    public void AddDomainEvent(IDomainEvent domainEvent)
     {
         _domainEvents.Add(domainEvent);
     }
