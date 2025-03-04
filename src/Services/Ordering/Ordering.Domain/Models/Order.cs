0abd4e157ae68056c2f0702098ca1296efd63d78
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-04 13:14:55 +0800

    feat: implement domain modeling for ordering microservice

---
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿namespace Ordering.Domain.Models;
+
+class Order : Aggregate<Guid>
+{
+    private readonly List<OrderItems> _orderItems = new();
+    public IReadOnlyList<OrderItems> OrderItems => _orderItems.AsReadOnly();
+
+    public Guid CustomerId { get; private set; } = default!;
+    public string OrderName { get; private set; } = default!;
+    public Address ShippingAddress { get; private set; } = default!;
+    public Address BillingAddress { get; private set; } = default!;
+    public Payment Payment { get; private set; } = default!;
+    public OrderStatus Status { get; private set; } = OrderStatus.Pending;
+
+    public decimal TotalPrice
+    {
+        get => OrderItems.Sum(x => x.Price * x.Quantity);
+        private set { }
+    }
+}
