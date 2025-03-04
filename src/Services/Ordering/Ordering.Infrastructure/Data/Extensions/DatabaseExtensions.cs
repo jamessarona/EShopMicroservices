e58715d7665af6979d7f0ae309c0a8eb933bba63
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-04 17:02:51 +0800

    feat: add seeding of customer initial data for development purpose

--- src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -13,5 +13,21 @@ public static class DatabaseExtensions
 
         context.Database.MigrateAsync().GetAwaiter().GetResult();
 
+        await SeedAsync(context);
+    }
+
+    private static async Task SeedAsync(ApplicationDbContext context)
+    {
+        await SeedCustomerAsync(context); 
+    }
+
+    private static async Task SeedCustomerAsync(ApplicationDbContext context)
+    {
+        if (!await context.Customers.AnyAsync())
+        {
+            await context.Customers.AddRangeAsync(InitialData.Customers);
+            await context.SaveChangesAsync();
+        }
+    }
     }
 }
