0739bee37003a4e5ea5fd46785e6e360de27ebf6
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-04 14:03:39 +0800

    feat: add constructor and validation for order name

--- src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -2,5 +2,14 @@
 
 public record OrderName
 {
+    private const int DefaultLenth = 5;
     public string Value { get; }
+    private OrderName(string value) => Value = value;
+    public static OrderName Of(string value)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(value);
+        ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length, DefaultLenth);
+
+        return new OrderName(value);
+    }
 }
