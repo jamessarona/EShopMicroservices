a41f7bcfa8822b53ce20fbc159b6c1c40ab6c728
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-04 16:35:53 +0800

    feat: migrate ApplicationDbContext in Ordering.Infrastructure

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -15,3 +15,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.Run();
+
+
+// Run in Package Manager Console for Migration Command
+// Add-Migration InitialCreate -OutputDir Data/Migrations -Project Ordering.Infrastructure -StartupProject Ordering.API
+// Update-Database
