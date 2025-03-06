defb32f6a7f7ca0bf2e19b4a66454a5fc79f32a3
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-06 17:45:31 +0800

    feat: configure message broker credentials

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BuildingBlocks.Messaging.MassTransit;
+
+public static class Extensions
+{
+    public static IServiceCollection AddMessageBroke(this IServiceCollection services, Assembly? assembly = null)
+    {
+        // Implement RabbitMQ MassTransit configuration
+        return services;
+    }
+}
