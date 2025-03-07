497bc9df021b713d41ae0771c75d2e8aea0c158a
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-07 13:31:14 +0800

    feat: configure http and https port for YARP API Gateway

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services ti tge cibtauber.
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
 
 app.Run();
