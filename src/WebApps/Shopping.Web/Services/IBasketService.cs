bd403294305faf5646417b462e8e74585518e3a7
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-10 20:56:00 +0800

    fix: incorrect names and endpoint

--- src/WebApps/Shopping.Web/Services/IBasketService.cs
+++ src/WebApps/Shopping.Web/Services/IBasketService.cs
@@ -7,18 +7,18 @@ public interface IBasketService
     [Get("/basket-service/basket/{userName}")]
     Task<GetBasketResponse> GetBasket(string userName);
 
-    [Post("/basket-service/basket/")]
+    [Post("/basket-service/basket")]
     Task<StoreBasketResponse> StoreBasket(StoreBasketRequest request);
 
     [Delete("/basket-service/basket/{userName}")]
-    Task<DeleteBasketResponse> DeleteBasketResponse(string userName);
+    Task<DeleteBasketResponse> DeleteBasket(string userName);
 
     [Post("/basket-service/basket/checkout")]
     Task<CheckoutBasketResponse> CheckoutBasket(CheckoutBasketRequest request);
 
     public async Task<ShoppingCartModel> LoadUserBasket()
     {
-        // Get Basket If Not Exist Create New Basket with Default Logged In User Name: james
+        // Get Basket If Not Exist Create New Basket with Default Logged In User Name: swn
         var userName = "james";
         ShoppingCartModel basket;
 
@@ -27,8 +27,7 @@ public interface IBasketService
             var getBasketResponse = await GetBasket(userName);
             basket = getBasketResponse.Cart;
         }
-        catch (ApiException apiException) 
-            when (apiException.StatusCode == HttpStatusCode.NotFound)
+        catch (ApiException apiException) when (apiException.StatusCode == HttpStatusCode.NotFound)
         {
             basket = new ShoppingCartModel
             {
